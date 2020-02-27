c20f33195efa77fcb1390dfd55f5507fd35af1fb
silkyrie <32624451+silkyrie@users.noreply.github.com>
2020-02-26 20:55:31 -0500

    version 1
    for lab 1

---
+++ lab1NetDev/lab1NetDev/unitsShippedAverage.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class averageUnitsShipped
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.txtListBox = New System.Windows.Forms.TextBox()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.lblUnits = New System.Windows.Forms.Label()
+        Me.lblDay = New System.Windows.Forms.Label()
+        Me.ttp1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(111, 57)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(100, 20)
+        Me.txtInput.TabIndex = 1
+        Me.ttp1.SetToolTip(Me.txtInput, "units shipped input")
+        '
+        'txtListBox
+        '
+        Me.txtListBox.Location = New System.Drawing.Point(50, 111)
+        Me.txtListBox.Multiline = True
+        Me.txtListBox.Name = "txtListBox"
+        Me.txtListBox.ReadOnly = True
+        Me.txtListBox.Size = New System.Drawing.Size(208, 161)
+        Me.txtListBox.TabIndex = 5
+        Me.txtListBox.TabStop = False
+        Me.ttp1.SetToolTip(Me.txtListBox, "entered values")
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(19, 375)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnter.TabIndex = 2
+        Me.btnEnter.Text = "&Enter"
+        Me.ttp1.SetToolTip(Me.btnEnter, "add a new value")
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnReset.Location = New System.Drawing.Point(136, 375)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 3
+        Me.btnReset.Text = "&Reset"
+        Me.ttp1.SetToolTip(Me.btnReset, "clear all values")
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(217, 375)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "&Close"
+        Me.ttp1.SetToolTip(Me.btnExit, "close the program")
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblOutput
+        '
+        Me.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput.Location = New System.Drawing.Point(50, 305)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(208, 15)
+        Me.lblOutput.TabIndex = 6
+        Me.lblOutput.Text = "                                             "
+        Me.lblOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.ttp1.SetToolTip(Me.lblOutput, "average units shipped per day")
+        '
+        'lblUnits
+        '
+        Me.lblUnits.AutoSize = True
+        Me.lblUnits.Location = New System.Drawing.Point(47, 60)
+        Me.lblUnits.Name = "lblUnits"
+        Me.lblUnits.Size = New System.Drawing.Size(34, 13)
+        Me.lblUnits.TabIndex = 0
+        Me.lblUnits.Text = "&Units:"
+        '
+        'lblDay
+        '
+        Me.lblDay.AutoSize = True
+        Me.lblDay.Location = New System.Drawing.Point(238, 60)
+        Me.lblDay.Name = "lblDay"
+        Me.lblDay.Size = New System.Drawing.Size(35, 13)
+        Me.lblDay.TabIndex = 7
+        Me.lblDay.Text = "Day 1"
+        '
+        'averageUnitsShipped
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnReset
+        Me.ClientSize = New System.Drawing.Size(352, 468)
+        Me.Controls.Add(Me.lblDay)
+        Me.Controls.Add(Me.lblUnits)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.txtListBox)
+        Me.Controls.Add(Me.txtInput)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Name = "averageUnitsShipped"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Average Units Shipped"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents txtListBox As TextBox
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblOutput As Label
+    Friend WithEvents lblUnits As Label
+    Friend WithEvents lblDay As Label
+    Friend WithEvents ttp1 As ToolTip
+End Class
---
+++ lab1NetDev/lab1NetDev/unitsShippedAverage.vb
@@ -0,0 +1,47 @@
+﻿Option Strict On
+Public Class averageUnitsShipped
+    ''Declarations
+    Dim count As Integer = 0
+    Dim input As Integer = 0
+    Dim average As Double = 0
+    Dim unitStats(6) As Integer
+    '' when the user uses enter utton to add another number to the running total
+    '' if the day 7 number gets entered it calculates average,displays,then disables input
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+        If (count < unitStats.Length And Int32.TryParse(txtInput.Text, input) And input > 0 And input < Integer.MaxValue) Then ''validation
+            txtListBox.Text = txtListBox.Text + input.ToString() + Environment.NewLine
+            unitStats(count) = input
+            count += 1
+            txtInput.Text = ""
+            If (count + 1 > unitStats.Length) Then ''output/calculation begin if true
+                btnEnter.Enabled = False
+                For i As Integer = 0 To unitStats.Length - 1
+                    average += unitStats(i)
+                Next
+                lblDay.Text = ""
+                txtInput.Enabled = False
+                average = average / unitStats.Length
+                lblOutput.Text = "average units shipped each day in week: " + average.ToString()
+            Else
+                lblDay.Text = "day" + (count + 1).ToString() '' change the day if its not day 7
+            End If
+        End If
+
+    End Sub
+    '' resets all controls and vairables to default
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        count = 0
+        input = 0
+        average = 0
+        lblDay.Text = "day1"
+        lblOutput.Text = ""
+        txtInput.Text = ""
+        txtListBox.Text = ""
+        btnEnter.Enabled = True
+        txtInput.Enabled = True
+    End Sub
+    '' ME CLOSE
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
