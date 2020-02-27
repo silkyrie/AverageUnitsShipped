e3ffbc652cd3166a16571ae1a5a8e0d186ecee4d
silkyrie <32624451+silkyrie@users.noreply.github.com>
2020-02-26 23:15:57 -0500

    3 employees now

--- lab3NetDev/UnitsShippedAverage/My Project/Application.Designer.vb
+++ lab3NetDev/UnitsShippedAverage/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.lab1NetDev.averageUnitsShipped
+            Me.MainForm = Global.lab1NetDev.frmAverageUnitsShipped
         End Sub
     End Class
 End Namespace
